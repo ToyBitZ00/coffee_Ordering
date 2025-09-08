617a5a349ce275f1a66b07d9880d431dcd84bb02
ToyBitZ00 <pa6308191@gmail.com>
2025-09-08 16:48:47 +0800

    Encapsulated the switch case and if else statements inside a Do While loop to allow repeated orders from the user and outputs the accumulated total price of orders from user

--- coffeeOrdering.java
+++ coffeeOrdering.java
@@ -17,75 +17,85 @@ public class coffeeOrdering {
         System.out.println(" 3       Coffee Matcha       150          200");
         System.out.println(" 4       Coffee Brewed       100          120");
 
-        System.out.print("\nItem Code: ");
-        int code = obj.nextInt();
-        switch (code) {
-            case 1:
-                System.out.println("Item name: Coffee Latte");
-                System.out.print("Item size: ");
-                size = obj.next().toLowerCase();
-                if (size.equals("med")) {
-                    System.out.println("Item price = 100");
-                    price = 100;
-                } else if (size.equals("large")) {
-                    System.out.println("Item price = 150");
-                    price = 150;
-                } else {
-                    System.out.println("Invalid Size");
+        do {
+            System.out.print("\nItem Code: ");
+            int code = obj.nextInt();
+            switch (code) {
+                case 1:
+                    System.out.println("Item name: Coffee Latte");
+                    System.out.print("Item size: ");
+                    size = obj.next().toLowerCase();
+                    if (size.equals("med")) {
+                        System.out.println("Item price = 100");
+                        price = 100;
+                    } else if (size.equals("large")) {
+                        System.out.println("Item price = 150");
+                        price = 150;
+                    } else {
+                        System.out.println("Invalid Size");
+                        return;
+                    }
+                    break;
+                case 2:
+                    System.out.println("Item name: Coffee Mocha");
+                    System.out.print("Item size: ");
+                    size = obj.next().toLowerCase();
+                    if (size.equals("med")) {
+                        System.out.println("Item price = 120");
+                        price = 120;
+                    } else if (size.equals("large")) {
+                        System.out.println("Item price = 180");
+                        price = 180;
+                    } else {
+                        System.out.println("Invalid Size");
+                        return;
+                    }
+                    break;
+                case 3:
+                    System.out.println("Item name: Coffee Matcha");
+                    System.out.print("Item size: ");
+                    size = obj.next().toLowerCase();
+                    if (size.equals("med")) {
+                        System.out.println("Item price = 150");
+                        price = 150;
+                    } else if (size.equals("large")) {
+                        System.out.println("Item price = 200");
+                        price = 200;
+                    } else {
+                        System.out.println("Invalid Size");
+                        return;
+                    }
+                    break;
+                case 4:
+                    System.out.println("Item name: Coffee Brewed");
+                    System.out.print("Item size: ");
+                    size = obj.next().toLowerCase();
+                    if (size.equals("med")) {
+                        System.out.println("Item price = 100");
+                        price = 100;
+                    } else if (size.equals("large")) {
+                        System.out.println("Item price = 120");
+                        price = 120;
+                    } else {
+                        System.out.println("Invalid Size");
+                        return;
+                    }
+                    break;
+                default:
+                    System.out.println("Wrong input");
+                    obj.close();
                     return;
-                }
-                break;
-            case 2:
-                System.out.println("Item name: Coffee Mocha");
-                System.out.print("Item size: ");
-                size = obj.next().toLowerCase();
-                if (size.equals("med")) {
-                    System.out.println("Item price = 120");
-                    price = 120;
-                } else if (size.equals("large")) {
-                    System.out.println("Item price = 180");
-                    price = 180;
-                } else {
-                    System.out.println("Invalid Size");
-                    return;
-                }
-                break;
+            }
+            System.out.print("Quantity: ");
+            quantity = obj.nextInt();
+            total = quantity * price;
+            final_Order += total;
 
-            case 3:
-                System.out.println("Item name: Coffee Matcha");
-                System.out.print("Item size: ");
-                size = obj.next().toLowerCase();
-                if (size.equals("med")) {
-                    System.out.println("Item price = 150");
-                    price = 150;
-                } else if (size.equals("large")) {
-                    System.out.println("Item price = 200");
-                    price = 200;
-                } else {
-                    System.out.println("Invalid Size");
-                    return;
-                }
-                break;
-            case 4:
-                System.out.println("Item name: Coffee Brewed");
-                System.out.print("Item size: ");
-                size = obj.next().toLowerCase();
-                if (size.equals("med")) {
-                    System.out.println("Item price = 100");
-                    price = 100;
-                } else if (size.equals("large")) {
-                    System.out.println("Item price = 120");
-                    price = 120;
-                } else {
-                    System.out.println("Invalid Size");
-                    return;
-                }
-                break;
-            default:
-                System.out.println("Wrong input");
-                obj.close();
-                return;
-        }
-        obj.close();
+            System.out.println("Total: " + final_Order);
+            System.out.print("Order Again [Y/N]: ");
+            ans = obj.next().toLowerCase();
+        } while (ans.equals("y"));
+
+        System.out.println("Total Order Price: " + final_Order);
     }
 }
